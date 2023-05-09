8103af06e5e46fef566cb2a3428a2732d2b52a44
CanzzD <canoozzdem1r@gmail.com>
2023-05-09 20:57:55 +0300

    Add Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_diet_app/ui/pages/login_page.dart';
 import 'package:flutter_diet_app/ui/pages/profile_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
